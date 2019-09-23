1b901c91db9069f8e9fdab9e143701a7927b6e28
miguelorjuela45 <miguel-orjuelar@unilibre.edu.co>
2019-09-22 23:22:32 -0500

    se realizo ajuste en el controler debido a error

--- src/main/java/co/edu/unilibre/pedido/config/RestConfig.java
+++ src/main/java/co/edu/unilibre/pedido/config/RestConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RestConfig {
 	@Bean("clientRest")
 	public RestTemplate registryRestTemplate() {
