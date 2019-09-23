36da9c19299c13e6f44bf3cce59b66d63315b7d1
miguelorjuela45 <miguel-orjuelar@unilibre.edu.co>
2019-09-22 23:09:25 -0500

    Se restauro un commit

--- src/main/java/co/edu/unilibre/pedido/config/RestConfig.java
+++ src/main/java/co/edu/unilibre/pedido/config/RestConfig.java
@@ -1,10 +1,10 @@
 package co.edu.unilibre.pedido.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public class RestConfig {
 	@Bean("clientRest")
 	public RestTemplate registryRestTemplate() {
--- src/main/java/co/edu/unilibre/pedido/controller/PedidoController.java
+++ src/main/java/co/edu/unilibre/pedido/controller/PedidoController.java
@@ -3,7 +3,6 @@ package co.edu.unilibre.pedido.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +13,14 @@ import co.edu.unilibre.pedido.service.PedidoService;
 @RestController
 public class PedidoController {
 <<<<<<< HEAD
+<<<<<<< HEAD
 	@Autowired
 	private PedidoService pedidoService;
 =======
 	@Autowired(required = true)
+=======
+	@Autowired
+>>>>>>> parent of f4cef5b... se corrigio modelo y config
 	private IPedidoService pedidoService;
 >>>>>>> f4cef5b78a721e40540ca7c530187a4707e4dac4
 	
@@ -29,10 +32,5 @@ public class PedidoController {
 	@GetMapping("/{id}/{cantidad}")
 	public Pedido getById(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return pedidoService.findById(id, cantidad);
-	}
-	
-	@GetMapping("/{id}/{quantity}")
-	public Pedido getById(@PathVariable Long id, @PathVariable Integer quantity) {
-		return pedidoService.findById(id, quantity);
 	}
 }
