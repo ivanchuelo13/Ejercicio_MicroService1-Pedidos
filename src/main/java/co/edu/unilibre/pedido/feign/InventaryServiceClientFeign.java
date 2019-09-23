c8c66cd1e5d422c9e9696c5e254ec10a0ed73a80
miguelorjuela45 <miguel-orjuelar@unilibre.edu.co>
2019-09-22 22:53:23 -0500

    necesito subir

--- src/main/java/co/edu/unilibre/pedido/feign/InventaryServiceClientFeign.java
+++ src/main/java/co/edu/unilibre/pedido/feign/InventaryServiceClientFeign.java
@@ -2,13 +2,13 @@ package co.edu.unilibre.pedido.feign;
 
 import java.util.List;
 
-import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cl
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import co.edu.unilibre.pedido.model.Inventary;
 
-@FeignClient(name = "product-service", url = "localhost:10010")
+@FeignClient (name = "product-service", url = "localhost:10010")
 public interface InventaryServiceClientFeign {
 	
 	@GetMapping("/list")
--- src/main/java/co/edu/unilibre/pedido/model/Pedido.java
+++ src/main/java/co/edu/unilibre/pedido/model/Pedido.java
@@ -1,7 +1,5 @@
 package co.edu.unilibre.pedido.model;
 
-import java.io.Serializable;
-
 public class Pedido implements Serializable{
 	
 }
